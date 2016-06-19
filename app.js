92187153a922f308ecd3bceb2016219e31b5240b
Cyrus Shahrivar <cyrus.shahrivar@gmail.com>
2016-06-19 12:40:58 -0400

    finished step 3, added wall collision detection and random color changer

--- app.js
+++ app.js
@@ -6,11 +6,16 @@ window.onload = function () {
   var y = canvas.height-30;
   var dx = Math.random()*2;
   var dy = -2;
+  var ballRadius = 10;
+  ctx.fillStyle = '#0095DD';
+  var randomR = Math.floor(Math.random()*255);
+  var randomG = Math.floor(Math.random()*255);
+  var randomB = Math.floor(Math.random()*255);
 
   function drawBall() {
     ctx.beginPath();
-    ctx.arc(x,y,10,0, Math.PI*2);
-    ctx.fillStyle = '#0095DD';
+    ctx.arc(x,y,ballRadius,0, Math.PI*2);
+    // ctx.fillStyle = '#0095DD';
     ctx.fill();
     ctx.closePath();
   }
@@ -19,6 +24,25 @@ window.onload = function () {
     // drawing code
     ctx.clearRect(0,0, canvas.width, canvas.height);
     drawBall();
+
+    if (y+dy < ballRadius || y+dy > canvas.height - ballRadius){
+      randomR = Math.floor(Math.random()*255);
+      randomG = Math.floor(Math.random()*255);
+      randomB = Math.floor(Math.random()*255);
+      dy = -dy;
+      ctx.fillStyle = 'rgb(' + randomR + ',' + randomG + ',' + randomB + ')';
+      // ctx.fill();
+    }
+
+    if (x+dx < ballRadius || x+dx > canvas.width - ballRadius){
+      randomR = Math.floor(Math.random()*255);
+      randomG = Math.floor(Math.random()*255);
+      randomB = Math.floor(Math.random()*255);
+      dx = -dx;
+      ctx.fillStyle = 'rgb(' + randomR + ',' + randomG + ',' + randomB + ')';
+      // ctx.fill();
+    }
+
     x += dx;
     y += dy;
   }
